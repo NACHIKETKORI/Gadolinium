9215ebcdcfe41ade828be4d9fd05cdbfe47e071a
Nachiket2501 <120778265+NACHIKETKORI@users.noreply.github.com>
2025-03-24 16:57:35 +0530

    refactor: standardize package name casing and improve authentication route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import "dotenv/config";
 import { hono } from "./routes";
 import { serve } from "@hono/node-server";
 
-serve(hono, (info) => {
-  console.log(`Server is running on port ${info.port}`);
-});
+serve(hono);
+
+console.log("Server is running on port 3000");
